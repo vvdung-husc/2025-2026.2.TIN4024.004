c0942f899acc55c5ed95f7d47ce118e867fea9e5
ChauVanTruongHuy <22t1020156@husc.edu.vn>
2026-02-06 14:18:43 +0700

    update

--- TEAM_16/ESP32_DHT_OLED/src/main.cpp
+++ TEAM_16/ESP32_DHT_OLED/src/main.cpp
@@ -1,7 +1,7 @@
 // THÔNG TIN NHÓM 16
-// 1. Nguyễn Văn Bình
-// 2. Lê Nguyễn Hương Nguyên
-// 3. ...
+// 1.Châu Kỳ
+// 2.Nguyễn ÁI Danh
+// 3 Châu Văn Trường Huy
 
 #include <Arduino.h>
 #include <DHT.h>
