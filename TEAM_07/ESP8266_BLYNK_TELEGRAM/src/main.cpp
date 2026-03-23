bd81288dc1b81602f3a1d2aecf35508ff14ea6e4
Nhi_22t1020693 <22t1020693@husc.edu.vn>
2026-03-23 14:42:32 +0700

    up

--- TEAM_07/ESP8266_BLYNK_TELEGRAM/src/main.cpp
+++ TEAM_07/ESP8266_BLYNK_TELEGRAM/src/main.cpp
@@ -2,7 +2,7 @@
 THÔNG TIN NHÓM 07
 1. Đào Thị Thùy Dương
 2. Bùi Quang Quý
-3. ...
+3. Đặng Thị Tâm Nhi
 */
 
 #define BLYNK_PRINT Serial
@@ -119,4 +119,73 @@ void updateOLED() {
   display.println("Team 07");
 
   display.display();
+}
+// ===== BLYNK =====
+  Blynk.virtualWrite(V0, millis() / 1000); // uptime
+  Blynk.virtualWrite(V2, temp);
+  Blynk.virtualWrite(V3, hum);
+  Blynk.virtualWrite(V4, gas);
+  Blynk.virtualWrite(V5, "Team 07");
+
+  // ===== TELEGRAM ALERT =====
+  if (abs(temp - lastTemp) > 1 || abs(hum - lastHum) > 3) {
+
+    lastTemp = temp;
+    lastHum = hum;
+
+    String msg = "Thay đổi!\nNhiệt độ: " + String(temp) +
+                 "\nĐộ ẩm: " + String(hum);
+    bot.sendMessage(GROUP_ID, msg, "");
+  }
+  // GAS ALERT
+  if (gas > 70 && millis() - lastGasAlert > 10000) {
+  lastGasAlert = millis();
+
+  String msg = "Cảnh báo khí gas vượt ngưỡng!\n";
+  msg += "Gas: " + String(gas);
+
+  bot.sendMessage(GROUP_ID, msg, "");
+}
+}
+
+// ===== SETUP =====
+void setup() {
+  Serial.begin(115200);
+//PIN 
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+//OLED 
+  Wire.begin(OLED_SDA, OLED_SCL);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("OLED FAIL");
+    while (1);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+//kết nối wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting WiFi");
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+    Serial.print(".");
+//timeout sau 10s
+    if (millis() - startTime > 10000) {
+      Serial.println("\nWiFi Connection Failed");
+      break;//thoát vòng lặp nếu không kết nối được
+    }
+  }
+  Serial.println("\nWiFi OK");
+//thiết lập kết nối Blynk
+  client.setInsecure();
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASS);
+//khởi động cảm biến DHT
+  dht.begin();
+//thiết lập timer cho nhiệm vụ định kỳ
+  timer.setInterval(2000L, taskAll);
+//gửi thông báo khởi động
+  bot.sendMessage(GROUP_ID, "STARTED \nHồ Thị Thanh Bình \nĐào Thị Thùy Dương \nĐặng Thị Tâm Nhi \n Huỳnh Thị Thủy \nBùi Quang Quý", "");
 }
