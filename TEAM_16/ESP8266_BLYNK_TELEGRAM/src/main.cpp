8c4dd57a7ce82c9cb34409a8ea899c99b70aed6f
Trương An Khang <22t1020167@husc.edu.vn>
2026-03-25 20:58:57 +0700

    up

--- TEAM_16/ESP8266_BLYNK_TELEGRAM/src/main.cpp
+++ TEAM_16/ESP8266_BLYNK_TELEGRAM/src/main.cpp
@@ -3,7 +3,7 @@
   1. Châu Kỳ - Telegram: @chauky_2k4
   2. Nguyễn Ái Danh
   3. Châu Văn Trường Huy
-  4. Trương An Khang
+  4. Trương An Khang - Telegram: @khang678900
   5. Phan Văn Việt
 */
 #define BLYNK_PRINT Serial
