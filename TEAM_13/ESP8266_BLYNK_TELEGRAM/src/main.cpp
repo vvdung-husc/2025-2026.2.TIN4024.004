38e8132b51ab7a2566dacb024b5bafce9ed29d7c
thuyngan06012004 <ngan71770@gmail.com>
2026-03-25 14:35:42 +0700

    up  name tele

--- TEAM_13/ESP8266_BLYNK_TELEGRAM/src/main.cpp
+++ TEAM_13/ESP8266_BLYNK_TELEGRAM/src/main.cpp
@@ -2,7 +2,7 @@
 THÔNG TIN NHÓM 13
 1.Trần Tuấn Long - Telegram : longtrandevs
 2.Nguyễn Thị Huyền Trân - Telegram : nthtran
-3.Nguyễn Thị Thùy Ngân
+3.Nguyễn Thị Thùy Ngân - Telegram : Ngan19042004
 4.Ngô Nghĩa
 */
 
