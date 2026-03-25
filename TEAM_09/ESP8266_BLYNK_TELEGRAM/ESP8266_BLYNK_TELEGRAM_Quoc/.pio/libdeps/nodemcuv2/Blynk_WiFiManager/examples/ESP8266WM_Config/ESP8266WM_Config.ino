9aef6828eae984be04ed86a89904909429fc23e0
quoc374 <22T1020374@husc.edu.vn>
2026-03-25 14:52:10 +0700

    1

---
+++ TEAM_09/ESP8266_BLYNK_TELEGRAM/ESP8266_BLYNK_TELEGRAM_Quoc/.pio/libdeps/nodemcuv2/Blynk_WiFiManager/examples/ESP8266WM_Config/ESP8266WM_Config.ino
@@ -0,0 +1,210 @@
+/****************************************************************************************************************************
+   ESP8266WM_Config.ino
+   For ESP8266 boards
+
+   Blynk_WM is a library for the ESP8266/ESP32 Arduino platform (https://github.com/esp8266/Arduino) to enable easy
+   configuration/reconfiguration and autoconnect/autoreconnect of WiFi/Blynk
+   Forked from Blynk library v0.6.1 https://github.com/blynkkk/blynk-library/releases
+   Built by Khoi Hoang https://github.com/khoih-prog/Blynk_WM
+   Licensed under MIT license
+   Version: 1.0.16
+
+   Version    Modified By   Date      Comments
+   -------    -----------  ---------- -----------
+    1.0.0     K Hoang      28/10/2019 Initial coding
+    1.0.1     K Hoang      28/10/2019 Add features
+    1.0.2     K Hoang      21/11/2019 Fix bug. Add features.
+    1.0.3     K Hoang      31/11/2019 Fix compiler errors for ESP8266 core pre-2.5.2. Add examples.
+    1.0.4     K Hoang      07/01/2020 Add configurable personalized RFC-952 DHCP hostname
+    1.0.5     K Hoang      20/01/2020 Add configurable static IP, GW, SN, DNS1, DNS2 and Config Portal static IP and Credentials
+    1.0.6     K Hoang      05/02/2020 Optimize, fix EEPROM size to 2K from 4K, shorten code size, add functions
+    1.0.7     K Hoang      18/02/2020 Add checksum, enable AutoConnect to configurable MultiWiFi and MultiBlynk Credentials
+    1.0.8     K Hoang      24/02/2020 Fix AP-staying-open bug. Add clearConfigData()
+    1.0.9     K Hoang      12/03/2020 Enhance Config Portal GUI
+    1.0.10    K Hoang      08/04/2020 SSID password maxlen is 63 now. Permit special chars # and % in input data.
+    1.0.11    K Hoang      09/04/2020 Enable adding dynamic custom parameters from sketch
+    1.0.12    K Hoang      13/04/2020 Fix MultiWiFi/Blynk bug introduced in broken v1.0.11
+    1.0.13    K Hoang      25/04/2020 Add Configurable Config Portal Title, Default Config Data and DRD. Update examples.
+    1.0.14    K Hoang      03/05/2020 Fix bug and change feature in dynamicParams.
+    1.0.15    K Hoang      12/05/2020 Fix bug and Update to use LittleFS for ESP8266 core 2.7.1+. Add example.
+    1.0.16    K Hoang      25/06/2020 Fix bug and logic of USE_DEFAULT_CONFIG_DATA. Auto format SPIFFS/LittleFS.
+ *****************************************************************************************************************************/
+
+#include "defines.h"
+#include "Credentials.h"
+#include "dynamicParams.h"
+
+#include <Ticker.h>
+#include <DHT.h>
+
+DHT dht(DHT_PIN, DHT_TYPE);
+BlynkTimer timer;
+Ticker     led_ticker;
+
+void readAndSendData()
+{
+  float temperature = dht.readTemperature();
+  float humidity    = dht.readHumidity();
+
+  if (Blynk.connected())
+  {
+    if (!isnan(temperature) && !isnan(humidity))
+    {
+      Blynk.virtualWrite(V17, String(temperature, 1));
+      Blynk.virtualWrite(V18, String(humidity, 1));
+    }
+    else
+    {
+      Blynk.virtualWrite(V17, "NAN");
+      Blynk.virtualWrite(V18, "NAN");
+    }
+  }
+
+  // Blynk Timer uses millis() and is still working even if WiFi/Blynk not connected
+  Serial.print("R");
+}
+
+void set_led(byte status)
+{
+  digitalWrite(LED_BUILTIN, status);
+}
+
+void heartBeatPrint(void)
+{
+  static int num = 1;
+
+  if (Blynk.connected())
+  {
+    set_led(LOW);
+    led_ticker.once_ms(111, set_led, (byte) HIGH);
+    Serial.print("B");
+  }
+  else
+  {
+    Serial.print("F");
+  }
+
+  if (num == 40)
+  {
+    Serial.println();
+    num = 1;
+  }
+  else if (num++ % 10 == 0)
+  {
+    Serial.print(" ");
+  }
+}
+
+void check_status()
+{
+  static unsigned long checkstatus_timeout = 0;
+
+#define STATUS_CHECK_INTERVAL     10000L
+
+  // Send status report every STATUS_REPORT_INTERVAL (60) seconds: we don't need to send updates frequently if there is no status change.
+  if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
+  {
+    // report status to Blynk
+    heartBeatPrint();
+
+    checkstatus_timeout = millis() + STATUS_CHECK_INTERVAL;
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  while (!Serial);
+
+  pinMode(PIN_LED, OUTPUT);
+
+#if ( USE_LITTLEFS || USE_SPIFFS)
+  Serial.print("\nStarting ESP8266WM_Config using " + String(CurrentFileFS));  
+#else
+  Serial.print("\nStarting ESP8266WM_Config using EEPROM");
+#endif  
+
+#if USE_SSL
+  Serial.println(" with SSL");
+#else
+  Serial.println(" without SSL");
+#endif
+      
+  dht.begin();
+
+  // From v1.0.5
+  // Set config portal SSID and Password
+  Blynk.setConfigPortal("TestPortal-ESP8266", "TestPortalPass");
+  // Set config portal IP address
+  Blynk.setConfigPortalIP(IPAddress(192, 168, 200, 1));
+  // Set config portal channel, default = 1. Use 0 => random channel from 1-13
+  Blynk.setConfigPortalChannel(0);
+
+  // From v1.0.5, select either one of these to set static IP + DNS
+  Blynk.setSTAStaticIPConfig(IPAddress(192, 168, 2, 220), IPAddress(192, 168, 2, 1), IPAddress(255, 255, 255, 0));
+  //Blynk.setSTAStaticIPConfig(IPAddress(192, 168, 2, 220), IPAddress(192, 168, 2, 1), IPAddress(255, 255, 255, 0),
+  //                           IPAddress(192, 168, 2, 1), IPAddress(8, 8, 8, 8));
+  //Blynk.setSTAStaticIPConfig(IPAddress(192, 168, 2, 220), IPAddress(192, 168, 2, 1), IPAddress(255, 255, 255, 0),
+  //                           IPAddress(4, 4, 4, 4), IPAddress(8, 8, 8, 8));
+
+  // Use this to default DHCP hostname to ESP8266-XXXXXX or ESP32-XXXXXX
+  //Blynk.begin();
+  // Use this to personalize DHCP hostname (RFC952 conformed)
+  // 24 chars max,- only a..z A..Z 0..9 '-' and no '-' as last char
+  Blynk.begin(HOST_NAME);
+
+  timer.setInterval(60 * 1000, readAndSendData);
+
+  if (Blynk.connected())
+  {
+#if ( USE_LITTLEFS || USE_SPIFFS)
+    Serial.println("\nBlynk ESP8288 using " + String(CurrentFileFS) + " connected. Board Name : " + Blynk.getBoardName());
+#else
+    {
+      Serial.println("\nBlynk ESP8288 using EEPROM connected. Board Name : " + Blynk.getBoardName());
+      Serial.printf("EEPROM size = %d bytes, EEPROM start address = %d / 0x%X\n", EEPROM_SIZE, EEPROM_START, EEPROM_START);
+    }
+#endif
+  }
+}
+
+#if USE_DYNAMIC_PARAMETERS
+void displayCredentials(void)
+{
+  Serial.println("\nYour stored Credentials :");
+
+  for (int i = 0; i < NUM_MENU_ITEMS; i++)
+  {
+    Serial.println(String(myMenuItems[i].displayName) + " = " + myMenuItems[i].pdata);
+  }
+}
+#endif
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  check_status();
+
+#if USE_DYNAMIC_PARAMETERS
+  static bool displayedCredentials = false;
+
+  if (!displayedCredentials)
+  {
+    for (int i = 0; i < NUM_MENU_ITEMS; i++)
+    {
+      if (!strlen(myMenuItems[i].pdata))
+      {
+        break;
+      }
+
+      if ( i == (NUM_MENU_ITEMS - 1) )
+      {
+        displayedCredentials = true;
+        displayCredentials();
+      }
+    }
+  }
+#endif
+}
