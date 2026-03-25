5d7d438c0fb1a4d7d5aa3fa10d64f25b052eee48
NguyenHuuTringf <nguyenhuutrinh1009@gmail.com>
2026-03-25 13:47:59 +0700

    name tele

--- TEAM_06/ESP8266_BLYNK_TELEGRAM/src/main .cpp
+++ TEAM_06/ESP8266_BLYNK_TELEGRAM/src/main .cpp
@@ -1,6 +1,6 @@
 /*
   Nhom 6
-  1. Nguyen Huu Trinh - 22T1020481
+  1. Nguyen Huu Trinh - 22T1020481 - @NgHuuTrungp
   2. Ho Sy Vinh - 22T1020521
   3. Nguyễn Đăng Bảo Toàn -22T1020470 - telegram : @toan2666607
   4. Nguyễn Đình Vĩnh Phú - 22T1020322
